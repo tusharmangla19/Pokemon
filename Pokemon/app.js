907a9e27d429fe29ef9d28c2c2db04b961f73b2e
tusharmangla19 <77486439+tusharmangla19@users.noreply.github.com>
2021-01-24 22:30:39 +0530

    Update app.js

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,5 +1,5 @@
 
-imgBase = " https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const imgBase = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 const container = document.querySelector('#container')
 
 for (let i = 1; i < 151; i++) {
